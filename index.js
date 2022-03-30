83f406ccabb14df82f6b465ab4b73999fbda08e7
Jufel Abecia <jufelabecia@gmail.com>
2022-03-30 21:26:12 +0800

    adding env

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
+require("dotenv").config();
 const express = require("express");
+const mysql = require("mysql2");
 const app = express();
+const server_port = 3001;
+
+const db = mysql.createConnection({
+    host: process.env.DATABASE_HOSTNAME,
+    user: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE,
+    port: process.env.DATABASE_PORT
+});
 
 
-app.listen(3000, (req, res) => {
-    console.log("Server is running at port 3000");
+
+app.listen(server_port, (req, res) => {
+    console.log("Server is running at port " + server_port);
+    db.connect((err) => {
+        if(err){
+            console.log("Error", err.message);
+        } else {
+            console.log("Database connection established");
+        }
+    });
 });
