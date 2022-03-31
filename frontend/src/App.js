a19a0456b2dacfba6b8634300e9bf2eb5bc8c37c
Elizabeth Santos <ECSantos1990@gmail.com>
2022-03-31 17:33:17 +0800

    first commit by zab

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
+import React from 'react'
+import { Homepage } from './components/Homepage'
 
-import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div>
+      <Homepage/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
